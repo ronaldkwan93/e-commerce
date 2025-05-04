2c9614de5bd41bf8b5cf57e4b9380e3f292761da
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-05-05 00:03:50 +1000

    add footer

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -4,19 +4,24 @@ import Cart from "../Pages/Cart/Cart";
 import NavBar from "../component/NavBar/NavBar";
 import Menu from "../Pages/Menu/Menu";
 import About from "../Pages/About/About";
+import Footer from "../component/Footer/Footer";
+import classes from './Layout.module.scss'
 
 const Layout = () => {
   return (
     <>
       <BrowserRouter>
-        <NavBar />
+        <div className={classes.container}>
+          <NavBar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+          <Footer />
+        </div>
       </BrowserRouter>
     </>
   );
--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,10 +1,32 @@
+import classes from "./Footer.module.scss";
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const year = new Date().getFullYear();
 
-export default Footer
+  return (
+    <div className={classes.footer}>
+      <div className={classes.container}>
+        <div>
+          <h3>Café on Click!</h3>
+          <p>
+            Serving quality coffee and delicious food in a cozy atmosphere since
+            2010.
+          </p>
+        </div>
+        <div>
+          <h3>Contact</h3>
+          <ul>
+            <li>123 Coffee Lane</li>
+            <li>Brewville, CA 94123</li>
+            <li>info@cafeonclick.com</li>
+          </ul>
+        </div>
+      </div>
+      <div className={classes.copyright}>
+        © {year} Café on Click! All rights reserved.
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
